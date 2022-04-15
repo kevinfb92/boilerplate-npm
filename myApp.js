4f7da32db423b56bc316606ca51e1a5335380021
unknown <kevinfb92@gmail.com>
2022-04-15 18:40:37 +0200

    serving html from at /

--- myApp.js
+++ myApp.js
@@ -3,12 +3,15 @@ var app = express();
 console.log("Hello World");
 
 
-app.get("/",function(req, res){
+/*app.get("/",function(req, res){
     res.send('Hello Express');
   });
   
+*/
 
-
+app.get("/", function(req, res){
+    res.send(__dirname+"views/index.html");
+})
 
 
 
