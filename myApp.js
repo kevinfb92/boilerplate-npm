5a9fac2b93e5c5600b0849be3d16aee980bcd278
unknown <kevinfb92@gmail.com>
2022-04-15 20:59:54 +0200

    middleware request logger

--- myApp.js
+++ myApp.js
@@ -3,14 +3,12 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
-
-/*app.get("/",function(req, res){
-    res.send('Hello Express');
-  });
-  
-*/
-
 app.use("/public", express.static(__dirname+"/public"));
+
+app.use(function(req, res, next){
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+})
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/views/index.html");
