1dfae77dbc431aef2b36f94f0967ce44ac487fea
unknown <kevinfb92@gmail.com>
2022-04-15 18:58:03 +0200

    serving static files;

--- myApp.js
+++ myApp.js
@@ -9,10 +9,11 @@ console.log("Hello World");
   
 */
 
-app.get("/", function(req, res){
-    res.send(__dirname+"views/index.html");
-})
+app.use("/public", express.static(__dirname+"/public"));
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/views/index.html");
+})
 
 
 
