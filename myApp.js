40a3ab0a162d73a888603a8e7494117b815abbdf
unknown <kevinfb92@gmail.com>
2022-04-16 21:49:30 +0200

    remove many by name

--- myApp.js
+++ myApp.js
@@ -106,8 +106,12 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, (err,data)=>{
+    if(err){
+      console.error(err);
+    }
+    done(null, data);
+  })
 };
 
 const queryChain = (done) => {
