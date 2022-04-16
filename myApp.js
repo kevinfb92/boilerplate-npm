49e02bc3f1346964515a8ec45ff336ee34dda6f6
unknown <kevinfb92@gmail.com>
2022-04-16 21:44:52 +0200

    remove by id

--- myApp.js
+++ myApp.js
@@ -96,7 +96,12 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data)=>{
+    if(err){
+      return console.error(err);
+    }
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
